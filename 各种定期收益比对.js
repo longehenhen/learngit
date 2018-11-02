db7d16b05571e0963f64a8c3a13c743d4f528eb0
long <807221943@qq.com>
2018-11-02 17:58:27 +0800

    修复循环多一次进行的BUG

--- 各种定期收益比对.js
+++ 各种定期收益比对.js
@@ -6,14 +6,17 @@
 function profit(period,rate,day){
 	var income=3000;
 	var i=day/period;
+	//console.log(i);
 	if(i<1){
 	return rate*100;
 }
 	else{
-	for(let j=0;j<i;j++){
+	for(let j=1;j<=i;j++){
 	income+=income*(rate/100/365)*period;
+	//console.log(income-3000);
 }
 	income+=income*(rate/100/365)*(day%period);
+	//console.log(income-3000);
 	return ((income-3000)/3000*100*365/day);
     }
 
